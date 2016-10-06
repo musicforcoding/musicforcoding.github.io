f7e11de6b78726f6f0028c24330d06609615f5fa
yoursgk <KirillDmitrievich@MacBook-Air-Kirill.local>
2016-10-06 13:23:03 +0600

    +++

--- js/index.js
+++ js/index.js
@@ -45,6 +45,10 @@ $(document).ready(function(){
 			mp3:"https://psv4.vk.me/c613731/u246173080/audios/f1ec51b84548.mp3"
 		},
         {
+			title:"Artemev - grust",
+			mp3:"https://psv4.vk.me/c4489/u53682749/audios/35cbc817e3a5.mp3"
+		},
+        {
 			title:"Artemev – Kur'er",
 			mp3:"https://psv4.vk.me/c6192/u155213172/audios/6ba995aa11e6.mp3"
 		},
@@ -53,7 +57,7 @@ $(document).ready(function(){
 			mp3:"https://cs3-1v4.vk-cdn.net/p17/d799bf12d0389f.mp3"
 		},
         {
-			title:"Kai Engel – Endless Story About Sun and Moon",
+			title:"Kai Engel – Story About Sun and Moon",
 			mp3:"https://psv4.vk.me/c613720/u322634362/audios/aaa714363cef.mp3"
 		},
         {
@@ -83,6 +87,22 @@ $(document).ready(function(){
         {
 			title:"Kai Engel – Idea",
 			mp3:"https://psv4.vk.me/c613720/u322634362/audios/5d67e7465928.mp3"
+		},
+        {
+			title:"Kai Engel – Low Horizon",
+			mp3:"https://psv4.vk.me/c611916/u6237243/audios/b9159c31cbb6.mp3"
+		},
+        {
+			title:"Kai Engel – Mist and Clouds",
+			mp3:"https://cs3-1v4.vk-cdn.net/p18/386b64f531e319.mp3"
+		},
+        {
+			title:"Kai Engel – Wake Up!",
+			mp3:"https://psv4.vk.me/c611916/u6237243/audios/c37c523bc1b9.mp3"
+		},
+        {
+			title:"Kai Engel – Dancing on the Edge",
+			mp3:"https://psv4.vk.me/c611916/u6237243/audios/c0d203d71248.mp3"
 		},
         {
 			title:"PILOTPRIEST – Matter",
@@ -153,7 +173,7 @@ $(document).ready(function(){
 			mp3:"https://psv4.vk.me/c611421/u256926301/audios/bf693891d370.mp3"
 		},
         {
-			title:"Stellaris OST – Faster Than Light (Feat. Mia Stegmar)",
+			title:"Stellaris OST – Faster Than Light",
 			mp3:"https://psv4.vk.me/c611920/u256926301/audios/53936c1e4028.mp3"
 		},
         {
@@ -181,11 +201,233 @@ $(document).ready(function(){
 			mp3:"https://psv4.vk.me/c536510/u139587128/audios/791d429e6518.mp3"
 		},
         {
-			title:"Cliff Martinez – Necrotic Bowel Resection",
+			title:"Cliff Martinez – Resection",
 			mp3:"https://psv4.vk.me/c613818/u41688806/audios/c54f60fe71f6.mp3"
 		},
-        
-        
+        {
+			title:"Moby – Flower",
+			mp3:"https://cs3-2v4.vk-cdn.net/p14/1291babe253370.mp3"
+		},
+        {
+			title:"Moby – One Of These Mornings",
+			mp3:"https://cs3-2v4.vk-cdn.net/p3/5d1aea7884dbc0.mp3"
+		},
+        {
+			title:"Moby – Extreme Ways",
+			mp3:"https://cs3-1v4.vk-cdn.net/p9/9f4d64d4fe4b69.mp3"
+		},
+        {
+			title:"U137 – Anemone",
+			mp3:"https://psv4.vk.me/c536409/u378026/audios/44725e2a14e7.mp3"
+		},
+        {
+			title:"Akira Yamaoka – Welcome to Silent Hill",
+			mp3:"https://cs3-1v4.vk-cdn.net/p15/28027352230b82.mp3"
+		},
+        {
+			title:"Akira Yamaoka – Lisa Die",
+			mp3:"https://psv4.vk.me/c1023/u3779076/audios/f7ff718b1121.mp3"
+		},
+        {
+			title:"Jeff Russo – Bemidji, MN",
+			mp3:"https://cs3-2v4.vk-cdn.net/p20/ac3284e551676d.mp3"
+		},
+        {
+			title:"Brian Eno – The Big Ship",
+			mp3:"https://psv4.vk.me/c5004/u1546305/audios/a2c3835f0e5a.mp3"
+		},
+        {
+			title:"Gustavo Santaolalla – The Last Of Us",
+			mp3:"https://cs3-2v4.vk-cdn.net/p15/9fb1d701ab9fbc.mp3"
+		},
+        {
+			title:"Gustavo Santaolalla – The Choice",
+			mp3:"https://cs3-2v4.vk-cdn.net/p17/78664089331cd2.mp3"
+		},
+        {
+			title:"Gustavo Santaolalla – The Path",
+			mp3:"https://psv4.vk.me/c536301/u19468351/audios/44ce2d4ec85a.mp3"
+		},
+        {
+			title:"Gustavo Santaolalla – Home",
+			mp3:"https://cs3-2v4.vk-cdn.net/p1/618e327669d74e.mp3"
+		},
+        {
+			title:"Jeremy Soule – Secunda",
+			mp3:"https://psv4.vk.me/c5825/u9703523/audios/c0f32b7a7a44.mp3"
+		},
+        {
+			title:"The End of the Ocean – Worth Everything",
+			mp3:"https://psv4.vk.me/c4976/u2204725/audios/fbdd014f5024.mp3"
+		},
+        {
+			title:"The End Of The Ocean – Setting Sail",
+			mp3:"https://psv4.vk.me/c4898/u5701450/audios/c73355e72a09.mp3"
+		},
+        {
+			title:"Duster – Gold Dust",
+			mp3:"https://psv4.vk.me/c521507/u20056524/audios/b05533f4a5d4.mp3"
+		},
+        {
+			title:"Scondi – Night City",
+			mp3:"https://psv4.vk.me/c611717/u42660230/audios/d50e7d4e8bca.mp3"
+		},
+        {
+			title:"99 FrankOFF – Island of Hope",
+			mp3:"https://psv4.vk.me/c613227/u42433384/audios/552f45ab9ed5.mp3"
+		},
+        {
+			title:"99 FrankOFF – Birds dream at Night",
+			mp3:"https://psv4.vk.me/c613818/u42433384/audios/511bd1d244e0.mp3"
+		},
+        {
+			title:"009 Sound System – Trinity",
+			mp3:"https://psv4.vk.me/c5005/u83454945/audios/6fc58b7f57b6.mp3"
+		},
+        {
+			title:"Power Glove – Power Core",
+			mp3:"https://psv4.vk.me/c521513/u203679369/audios/daf370163ca9.mp3"
+		},
+        {
+			title:"Power Glove – Blood Dragon",
+			mp3:"https://cs3-2v4.vk-cdn.net/p4/7fd83e8f76dc28.mp3"
+		},
+        {
+			title:"Max Quayle – Processing",
+			mp3:"https://cs3-2v4.vk-cdn.net/p24/363ac92da202f7.mp3"
+		},
+        {
+			title:"Ludovico Einaudi – Drop",
+			mp3:"https://cs3-1v4.vk-cdn.net/p19/13bdb8345376ce.mp3"
+		},
+        {
+			title:"Ludovico Einaudi – Elements",
+			mp3:"https://psv4.vk.me/c422224/u48331226/audios/95feca28b3eb.mp3"
+		},
+        {
+			title:"Ludovico Einaudi – Whirling Winds",
+			mp3:"https://cs3-2v4.vk-cdn.net/p18/4762adea4fbda4.mp3"
+		},
+        {
+			title:"Ludovico Einaudi – Twice",
+			mp3:"https://cs3-2v4.vk-cdn.net/p8/e3238232461fe3.mp3"
+		},
+        {
+			title:"Ludovico Einaudi – Numbers",
+			mp3:"https://cs3-2v4.vk-cdn.net/p17/aff7283b918221.mp3"
+		},
+        {
+			title:"tezce – apathy",
+			mp3:"https://psv4.vk.me/c611523/u283305193/audios/e0bfb5c28ffd.mp3"
+		},
+        {
+			title:"tezce – drown",
+			mp3:"https://psv4.vk.me/c611523/u283305193/audios/dfa7f82e44f6.mp3"
+		},
+        {
+			title:"tezce – i have no reason pt.2",
+			mp3:"https://psv4.vk.me/c611523/u283305193/audios/977c10dfa354.mp3"
+		},
+        {
+			title:"Atanas Valkov – For Honor",
+			mp3:"https://psv4.vk.me/c611519/u86635051/audios/89e87f72c9e5.mp3"
+		},
+        {
+			title:"John Williams – Hedwigs",
+			mp3:"https://cs3-1v4.vk-cdn.net/p8/f05b991d7be2e8.mp3"
+		},
+        {
+			title:"The Toxic Avenger – Make This Right",
+			mp3:"https://cs3-1v4.vk-cdn.net/p7/b4962cee9f4cd8.mp3"
+		},
+        {
+			title:"Ashtar Command – Deadman's Gun",
+			mp3:"https://cs3-1v4.vk-cdn.net/p21/b1a57f72886616.mp3"
+		},
+        {
+			title:"N'to – Trauma (worakls remix)",
+			mp3:"https://psv4.vk.me/c6133/u5255366/audios/7962f5c1a971.mp3"
+		},
+        {
+			title:"N'to–Time",
+			mp3:"https://cs3-2v4.vk-cdn.net/p9/99fada10e9e737.mp3"
+		},
+        {
+			title:"N'to – Utopia",
+			mp3:"https://cs3-1v4.vk-cdn.net/p24/9039f45fad0fc8.mp3"
+		},
+        {
+			title:"N'to – The Bosnian",
+			mp3:"https://psv4.vk.me/c6191/u88144527/audios/282f2de0317e.mp3"
+		},
+        {
+			title:"N'to – Dance Me",
+			mp3:"https://psv4.vk.me/c536219/u88144527/audios/72e326da1d9d.mp3"
+		},
+        {
+			title:"GK – Three",
+			mp3:"https://psv4.vk.me/c536311/u66311639/audios/b930c582385b.mp3"
+		},
+        {
+			title:"GK – Four",
+			mp3:"https://psv4.vk.me/c536513/u66311639/audios/b211c2ee9cc4.mp3"
+		},
+        {
+			title:"podval capella – sugar",
+			mp3:"https://cs3-1v4.vk-cdn.net/p23/f25e3f2f7a2026.mp3"
+		},
+        {
+			title:"podval capella – endorphin",
+			mp3:"https://cs3-1v4.vk-cdn.net/p20/d45ff09623903c.mp3"
+		},
+        {
+			title:"Run Vaylor – Steel Kingdom",
+			mp3:"https://psv4.vk.me/c611326/u72683745/audios/c443b34087cc.mp3"
+		},
+        {
+			title:"Run Vaylor – Robot Skin",
+			mp3:"https://psv4.vk.me/c611326/u72683745/audios/a6c95916379c.mp3"
+		},
+        {
+			title:"Really Slow Motion – Gender",
+			mp3:"https://psv4.vk.me/c536214/u27162156/audios/3c400bc219e5.mp3"
+		},
+        {
+			title:"Arcade Fire – Song On The Beach",
+			mp3:"https://psv4.vk.me/c6191/u8954795/audios/35243a725298.mp3"
+		},
+        {
+			title:"Bloc Party – Signs (Acoustic)",
+			mp3:"https://psv4.vk.me/c5007/u3135082/audios/af6520997cfe.mp3"
+		},
+        {
+			title:"No More Room in Hell – OST",
+			mp3:"https://cs3-1v4.vk-cdn.net/p9/7a7ab0bdca3834.mp3"
+		},
+        {
+			title:"Hans Zimmer – Cornfield Chase",
+			mp3:"https://cs3-1v4.vk-cdn.net/p14/5075e215ce4cc6.mp3"
+		},
+        {
+			title:"Hans Zimmer – Coward",
+			mp3:"https://cs3-1v4.vk-cdn.net/p17/029fac336959a5.mp3"
+		},
+        {
+			title:"Valiant Hearts – Main Theme",
+			mp3:"https://cs3-2v4.vk-cdn.net/p5/14383a6a262071.mp3"
+		},
+        {
+			title:"Henry Jackman – Reunited",
+			mp3:"https://cs3-1v4.vk-cdn.net/p18/8cf5c49a69e062.mp3"
+		},
+        {
+			title:"The Hit House – Nathan's Brain",
+			mp3:"https://psv4.vk.me/c613726/u65042827/audios/45ba26a2bf46.mp3"
+		},
+        {
+			title:"M83 – Moonchild",
+			mp3:"https://cs3-2v4.vk-cdn.net/p15/9c51c8042cfef6.mp3"
+		}
 		
 	], {
 		swfPath: "js/jplayer",
